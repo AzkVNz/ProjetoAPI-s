b0f11e9c0fbc5998c4bbf1fcd983b51ce8a9d96d
Vinícius <vnandrerosa@gmail.com>
2024-05-30 20:26:58 -0300

    Adicionado atributos a classe VeiculosPecas.java, com duas relações @ManyToOne, com @JoinColumn, para as entidades Veiculos.java, e Pecas.java, criado construtor, getters e setters, fala Equals, HashCode, e documentar

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/entities/VeiculosPecas.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/entities/VeiculosPecas.java
@@ -1,4 +1,50 @@
 package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class VeiculosPecas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @ManyToOne
+    @JoinColumn(name = "idChassi", referencedColumnName = "idChassi" )
+    private Veiculos veiculos;
+    @ManyToOne
+    @JoinColumn(name = "idPeca", referencedColumnName = "idPeca")
+    private Pecas pecas;
+    private Long quantidade;
+
+    public VeiculosPecas() {
+    }
+
+    public VeiculosPecas(Veiculos veiculos, Pecas pecas, Long quantidade) {
+        this.veiculos = veiculos;
+        this.pecas = pecas;
+        this.quantidade = quantidade;
+    }
+
+    public Veiculos getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(Veiculos veiculos) {
+        this.veiculos = veiculos;
+    }
+
+    public Pecas getPecas() {
+        return pecas;
+    }
+
+    public void setPecas(Pecas pecas) {
+        this.pecas = pecas;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
+    }
 }
