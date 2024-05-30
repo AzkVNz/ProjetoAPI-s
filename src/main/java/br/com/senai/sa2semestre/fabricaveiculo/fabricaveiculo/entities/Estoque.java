5415d44c414c9df680d8bf275e0ead433656868a
Vinícius <vnandrerosa@gmail.com>
2024-05-30 20:17:45 -0300

    Adicionado atributos a entidade Estoque.java, @ManyToOne ligação com a entidade Pecas.java, criado construtor, getters e setters, equals, hashcode e toString, ainda falta documentar

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/entities/Estoque.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/entities/Estoque.java
@@ -2,9 +2,77 @@ package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 
+@Entity
 public class Estoque {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long iDEstoque;
+    @ManyToOne
+    @JoinColumn(name = "idPeca", referencedColumnName = "idPeca")
+    private Pecas pecas;
+    private Long quantidadeDisponivel;
 
+    public Estoque() {
+    }
 
+    public Estoque(Long iDEstoque, Pecas pecas, Long quantidadeDisponivel) {
+        this.iDEstoque = iDEstoque;
+        this.pecas = pecas;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
 
+    public Long getiDEstoque() {
+        return iDEstoque;
+    }
+
+    public void setiDEstoque(Long iDEstoque) {
+        this.iDEstoque = iDEstoque;
+    }
+
+    public Pecas getPecas() {
+        return pecas;
+    }
+
+    public void setPecas(Pecas pecas) {
+        this.pecas = pecas;
+    }
+
+    public Long getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public void setQuantidadeDisponivel(Long quantidadeDisponivel) {
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Estoque estoque = (Estoque) o;
+
+        if (!iDEstoque.equals(estoque.iDEstoque)) return false;
+        if (!Objects.equals(pecas, estoque.pecas)) return false;
+        return Objects.equals(quantidadeDisponivel, estoque.quantidadeDisponivel);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = iDEstoque.hashCode();
+        result = 31 * result + (pecas != null ? pecas.hashCode() : 0);
+        result = 31 * result + (quantidadeDisponivel != null ? quantidadeDisponivel.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Estoque{" +
+                "iDEstoque=" + iDEstoque +
+                ", pecas=" + pecas +
+                ", quantidadeDisponivel=" + quantidadeDisponivel +
+                '}';
+    }
 }
