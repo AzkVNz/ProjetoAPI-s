f23dbd704f6114b5d4e7fb84d7504d48bb7682ce
Vinícius <vnandrerosa@gmail.com>
2024-05-30 20:13:55 -0300

    Criado VeiculosPecasRepository.java, para a entidade Veiculos.java, utilizando jPA repository

---
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/VeiculosPecasRepository.java
@@ -0,0 +1,6 @@
+package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VeiculosPecasRepository extends JpaRepository<VeiculosRepository, Long> {
+}
