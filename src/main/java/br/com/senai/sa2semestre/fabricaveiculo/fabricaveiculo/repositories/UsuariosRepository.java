9db19def7c8b9af179f5f81bd67200f8c622d404
Vinícius <vnandrerosa@gmail.com>
2024-05-31 01:20:45 -0300

    Antes estava UsuariosRepository, Long, foi ajustado para Usuario, Long

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/UsuariosRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariosRepository extends JpaRepository<UsuariosRepository, Long> {
-}
