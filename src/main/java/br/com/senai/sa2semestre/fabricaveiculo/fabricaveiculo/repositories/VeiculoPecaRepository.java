c75d4ebd2ae7c7246e33890296a4e35171ec4780
Vinícius <vnandrerosa@gmail.com>
2024-05-31 01:22:42 -0300

    Aleterado o nome para VeiculoPecaRepository.java

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/VeiculoPecaRepository.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/VeiculoPecaRepository.java
@@ -2,5 +2,5 @@ package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VeiculosPecasRepository extends JpaRepository<VeiculosRepository, Long> {
+public interface VeiculoPecaRepository  {
 }
