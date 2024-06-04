0cfc97a15afb59e246d99de11fc354dd75a42101
vinicius_a_rosa <vnandrerosa@gmail.com>
2024-06-03 21:33:51 -0300

    Criado o Controler para manutenção, com adicionar, atualizar, listar e excluir

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/controllers/ManutencaoController.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/controllers/ManutencaoController.java
@@ -4,11 +4,11 @@ import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Manutenca
 import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories.EquipamentoRepository;
 import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories.ManutencaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Controlador REST para gerenciamento de manutencoes.
@@ -23,9 +23,43 @@ public class ManutencaoController {
 
 
     @GetMapping
-    public List<Manutencao> getAllManutencao(){
+    public List<Manutencao> getAllManutencao() {
         return manutencaoRepository.findAll();
 
-}
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Manutencao> getManutencaoById(@PathVariable Long id) {
+        Optional<Manutencao> manutencaoBuscado = manutencaoRepository.findById(id);
+        return manutencaoBuscado.map(ResponseEntity::ok).orElseGet(() ->
+                ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    Manutencao createManutencao(@RequestBody Manutencao manutencao) {
+        return manutencaoRepository.save(manutencao);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Manutencao> uptadeManutencao(@PathVariable Long id, @RequestBody Manutencao manutencaoComDadosAtualizados) {
+        Optional<Manutencao> manutencaoExistente = manutencaoRepository.findById(id);
+        if (manutencaoExistente.isPresent()) {
+            manutencaoComDadosAtualizados.setiDManutencao(id);
+            return ResponseEntity.ok(manutencaoRepository.save(manutencaoComDadosAtualizados));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteManutencao(@PathVariable Long id) {
+        Optional<Manutencao> manutencaoParaDeletar = manutencaoRepository.findById(id);
+        if (manutencaoParaDeletar.isPresent()) {
+            manutencaoRepository.delete(manutencaoParaDeletar.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
