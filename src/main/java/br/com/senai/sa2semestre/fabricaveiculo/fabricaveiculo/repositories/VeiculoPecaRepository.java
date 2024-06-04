7fd73643f7f156f62b4d2304beb695955530e7c0
vinicius_a_rosa <vnandrerosa@gmail.com>
2024-06-04 19:42:28 -0300

    VeiculoPeca Respository foi exlcuido, pois estava dando erro no Long quantidade, então foi retirado o quantidade do banco

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/VeiculoPecaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories;
-
-import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.VeiculoPeca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VeiculoPecaRepository extends JpaRepository <VeiculoPeca, Long> {
-}
