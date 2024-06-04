9e7e13ef6292623b0d7b8f8ddaac5f29a60a0500
vinicius_a_rosa <vnandrerosa@gmail.com>
2024-06-03 21:31:50 -0300

    Alterado repository para VeiculoPeca.java

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/VeiculoPecaRepository.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/VeiculoPecaRepository.java
@@ -1,6 +1,7 @@
 package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories;
 
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.VeiculoPeca;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VeiculoPecaRepository  {
+public interface VeiculoPecaRepository extends JpaRepository <VeiculoPeca, Long> {
 }
