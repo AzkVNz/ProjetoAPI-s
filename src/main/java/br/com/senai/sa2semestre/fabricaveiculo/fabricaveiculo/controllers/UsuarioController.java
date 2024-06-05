956d3902fec6a56235af4983ac357f07b882bae5
vinicius_a_rosa <vnandrerosa@gmail.com>
2024-06-04 21:31:00 -0300

    Criado controller para entidade Usuario, com criar, listar, atualizar e excluir

---
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/controllers/UsuarioController.java
@@ -0,0 +1,60 @@
+package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.controllers;
+
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Peca;
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Qualidade;
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Usuario;
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> gettAllUsuario() {
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+        Optional<Usuario> usuarioBuscado = usuarioRepository.findById(id);
+        return usuarioBuscado.map(ResponseEntity::ok).orElseGet(() ->
+                ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> uptadeUsuario(@PathVariable Long id, @RequestBody Usuario usuarioComDadosAtualizados) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+        if (usuarioExistente.isPresent()) {
+            usuarioComDadosAtualizados.setIdUsuario(id);
+            return ResponseEntity.ok(usuarioRepository.save(usuarioComDadosAtualizados));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuarioParaExcluir = usuarioRepository.findById(id);
+        if (usuarioParaExcluir.isPresent()) {
+            usuarioRepository.delete(usuarioParaExcluir.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
