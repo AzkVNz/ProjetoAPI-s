5b36cd91b5b2ab12a09ecf91e0a6ea89ab07d3a6
vinicius_a_rosa <vnandrerosa@gmail.com>
2024-06-04 21:31:16 -0300

    Criado controller para entidade Veiculo, com criar, listar, atualizar e excluir

---
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/controllers/VeiculoController.java
@@ -0,0 +1,59 @@
+package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.controllers;
+
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Veiculo;
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories.VeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Vector;
+
+@RestController
+@RequestMapping("/veiculos")
+public class VeiculoController {
+
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+    @GetMapping
+    public List<Veiculo> getAllVeiculo() {
+        return veiculoRepository.findAll();
+    }
+
+    @GetMapping("/{chassi}")
+    public ResponseEntity<Veiculo> getPecaById(@PathVariable String chassi) {
+        Optional<Veiculo> veiculoBuscado = veiculoRepository.findById(chassi);
+        return veiculoBuscado.map(ResponseEntity::ok).orElseGet(() ->
+                ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/{chassi}")
+    public Veiculo createVeiculo(@RequestBody Veiculo veiculo) {
+        return veiculoRepository.save(veiculo);
+    }
+
+    @PutMapping("/{chassi}")
+    public ResponseEntity<Veiculo> uptadeVeiculo(@PathVariable String chassi, @RequestBody Veiculo veiculoComDadosAtualizados) {
+        Optional<Veiculo> veiculoExistente = veiculoRepository.findById(chassi);
+        if (veiculoExistente.isPresent()) {
+            veiculoComDadosAtualizados.setChassi(chassi);
+            return ResponseEntity.ok(veiculoRepository.save(veiculoComDadosAtualizados));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{chassi}")
+    public ResponseEntity<Veiculo> deleVeiculo(@PathVariable String chassi) {
+        Optional<Veiculo> veiculoParaExcluir = veiculoRepository.findById(chassi);
+        if (veiculoParaExcluir.isPresent()) {
+            veiculoRepository.delete(veiculoParaExcluir.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
