35378838af3a79fe5b15bcc08101247f19fa5c9b
Vinícius <vnandrerosa@gmail.com>
2024-06-05 21:12:51 -0300

    Nome Peca para Pecas

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/PecaRepository.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/repositories/PecaRepository.java
@@ -1,7 +1,7 @@
 package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.repositories;
 
-import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Peca;
+import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Pecas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PecaRepository extends JpaRepository<Peca, Long> {
+public interface PecaRepository extends JpaRepository<Pecas, Long> {
 }
