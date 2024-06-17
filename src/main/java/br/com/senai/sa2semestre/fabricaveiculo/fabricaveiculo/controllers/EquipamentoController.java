f811876f8dac7f56529de75a65eceb708751a372
vinicius_a_rosa <vnandrerosa@gmail.com>
2024-06-17 20:38:30 -0300

    Documentado em java doc

--- src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/controllers/EquipamentoController.java
+++ src/main/java/br/com/senai/sa2semestre/fabricaveiculo/fabricaveiculo/controllers/EquipamentoController.java
@@ -1,3 +1,6 @@
+/**
+ * Controlador REST para gerenciamento de equipamentos.
+ */
 package br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.controllers;
 
 import br.com.senai.sa2semestre.fabricaveiculo.fabricaveiculo.entities.Equipamento;
@@ -12,9 +15,8 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Controlador REST para gerenciamento de equipamentos.
+ * Controlador REST para manipulação CRUD de entidades Equipamento.
  */
-
 @RestController
 @RequestMapping("/equipamentos")
 public class EquipamentoController {
@@ -22,25 +24,25 @@ public class EquipamentoController {
     @Autowired
     private EquipamentoRepository equipamentoRepository;
 
-    /**
-     * Obtém todos os equipamentos.
-     * @return uma lista de equipamentos.
-     */
-
     @Autowired
     private ManutencaoRepository manutencaoRepository;
 
+    /**
+     * Endpoint para recuperar todos os equipamentos cadastrados.
+     *
+     * @return Lista de todos os equipamentos
+     */
     @GetMapping
     public List<Equipamento> getAllEquipamento() {
         return equipamentoRepository.findAll();
     }
 
     /**
-     * Obtém um equipamento por ID.
-     * @param id o ID do equipamento.
-     * @return o equipamento com o ID especificado.
+     * Endpoint para recuperar um equipamento específico pelo seu ID.
+     *
+     * @param id O ID do equipamento a ser recuperado
+     * @return ResponseEntity contendo o equipamento encontrado ou status 404 caso não encontrado
      */
-
     @GetMapping("/{id}")
     public ResponseEntity<Equipamento> getEquipamentoById(@PathVariable Long id) {
         Optional<Equipamento> equipamentoBuscado = equipamentoRepository.findById(id);
@@ -49,9 +51,10 @@ public class EquipamentoController {
     }
 
     /**
-     * Cria um novo equipamento.
-     * @param equipamento o novo equipamento.
-     * @return o equipamento criado.
+     * Endpoint para criar um novo equipamento.
+     *
+     * @param equipamento O equipamento a ser criado
+     * @return O equipamento criado
      */
     @PostMapping
     public Equipamento createEquipamento(@RequestBody Equipamento equipamento) {
@@ -59,15 +62,14 @@ public class EquipamentoController {
     }
 
     /**
-     * Atualiza um equipamento existente.
-     * @param id o ID do equipamento a ser atualizado.
-     * @param equipamentoComDadosAtualizados os novos dados do equipamento
-     * @return o equipamento atualizado.
+     * Endpoint para atualizar um equipamento existente pelo seu ID.
+     *
+     * @param id O ID do equipamento a ser atualizado
+     * @param equipamentoComDadosAtualizados O objeto contendo os novos dados do equipamento
+     * @return ResponseEntity contendo o equipamento atualizado ou status 404 caso não encontrado
      */
-
     @PutMapping("/{id}")
     public ResponseEntity<Equipamento> uptadeEquipamento(@PathVariable Long id, @RequestBody Equipamento equipamentoComDadosAtualizados) {
-
         Optional<Equipamento> equipamentoExistente = equipamentoRepository.findById(id);
         if (equipamentoExistente.isPresent()) {
             equipamentoComDadosAtualizados.setIdEquipamento(id);
@@ -78,11 +80,11 @@ public class EquipamentoController {
     }
 
     /**
-     * Exclui um equipamento por ID.
-     * @param id o ID do equipamento a ser excluído.
-     * @return uma resposta indicando o sucesso ou falha da operação.
+     * Endpoint para excluir um equipamento pelo seu ID, incluindo todas as suas manutenções associadas.
+     *
+     * @param id O ID do equipamento a ser excluído
+     * @return ResponseEntity indicando o resultado da operação de exclusão
      */
-
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteEquipamentoAndManutencoes(@PathVariable Long id) {
         Optional<Equipamento> equipamentoOptional = equipamentoRepository.findById(id);
@@ -103,6 +105,5 @@ public class EquipamentoController {
             return ResponseEntity.notFound().build();
         }
     }
-
 
 }
